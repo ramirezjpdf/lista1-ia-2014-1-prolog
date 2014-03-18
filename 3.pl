ca563b33a5d013df5455a9e991fb54c747cc039f
ramirezjpdf <ramirez.jpdf@ymail.com>
2014-03-18 20:37:26 -0300

    exercicio 3 feito

---
+++ 3.pl
@@ -0,0 +1,9 @@
+a) X = Y, Y = maria.
+b)Julio = Ana, X = ana.
+c)X = L, L = melancia, H = 8.
+d)X = Y, Y = joga(fluminense,W).
+e)X = estuda(ana, livro(matematica), Y = livro(matematica).
+f)error
+g)V = G, G = U, U = Fa, Fa = p(azul), S2 = Q, Q = D, D = azul.
+h)X = gosta(jorge, X).
+i)X = Y, Y = ganha(vitor,2500), Z = 2500.
